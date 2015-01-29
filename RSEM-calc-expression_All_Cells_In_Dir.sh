d4decfa98eaaf99b8f59c50184072907ff01fff6
Marcello DiStasio <ChellyD65@users.noreply.github.com>
2015-01-29 17:21:08 -0500

    Update RSEM-calc-expression_All_Cells_In_Dir.sh

--- RSEM-calc-expression_All_Cells_In_Dir.sh
+++ RSEM-calc-expression_All_Cells_In_Dir.sh
@@ -9,13 +9,15 @@ echo
 echo "Input directory: $inputDir"
 echo 
 
+reference_file=/path/to/reference_file
+
 find $inputDir -maxdepth 1 -name '*.sam' | while read filename; do
     name_base=`echo $filename | sed -r 's/\.[[:alnum:]]+\$//'`
     name1=$name_base"_rsem"
     name2=$name_base"_expression"
     if [ ! -f $name2".genes.results" ]; then
 	echo $name_base ": Submitting to queue.";
-	bsub -q short -n 6 -W 12:00 -e ~/.lsbatch_errors_rsem -o lsf_output.txt "~/Programs/Genomics_Tools/rsem/convert-sam-for-rsem $filename $name1; ~/Programs/Genomics_Tools/rsem/rsem-calculate-expression --bam --paired-end $name1.bam ~/becklab/jcjeong/references/ucsc/hg19/rsem/ucsc_hg19 $name2"
+	bsub -q short -n 6 -W 12:00 -e ~/.lsbatch_errors_rsem -o lsf_output.txt "convert-sam-for-rsem $filename $name1; rsem-calculate-expression --bam --paired-end $name1.bam $reference_file $name2"
     else
 	echo $name2".genes.results found!";
     fi
