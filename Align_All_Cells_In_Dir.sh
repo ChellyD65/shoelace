d42100c5f70874225c2704e7a7dff30dede2bace
Marcello DiStasio <ChellyD65@users.noreply.github.com>
2015-01-29 17:17:10 -0500

    Update Align_All_Cells_In_Dir.sh

--- Align_All_Cells_In_Dir.sh
+++ Align_All_Cells_In_Dir.sh
@@ -9,6 +9,8 @@ echo
 echo "Input directory: $inputDir"
 echo 
 
+reference_file=/path/to/rsem_reference
+
 find $inputDir -maxdepth 1 -name '*_1.fastq' | while read filename; do
     name_base=`echo $filename | sed -r 's/_1\.[[:alnum:]]+\$//'`
     echo $name_base
@@ -17,7 +19,7 @@ find $inputDir -maxdepth 1 -name '*_1.fastq' | while read filename; do
     name_out=$name_base".sam"
     if [ ! -f $name_out ]; then
 	echo $name_base ": Submitting to queue.";
-	bsub -q short -n 4 -W 12:00 -R "rusage[mem=12000]" -e ~/.lsbatch_errors -o lsf_output.txt "/home/md267/Programs/Genomics_Tools/bowtie/bowtie /home/md267/becklab/jcjeong/references/ucsc/hg19/rsem/ucsc_hg19 -n 0 -e 99999999 -l 25 -I 1 -X 2000 -a -m 15 -1 $name1 -2 $name2 -S $name_out;"
+	bsub -q short -n 4 -W 12:00 -R "rusage[mem=12000]" -e ~/.lsbatch_errors -o lsf_output.txt "$reference_file -n 0 -e 99999999 -l 25 -I 1 -X 2000 -a -m 15 -1 $name1 -2 $name2 -S $name_out;"
     else
 	echo $name_out " found!";
     fi
