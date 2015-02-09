811bf62498c6e92208b80351727ac541f655d0c2
Marcello DiStasio <md267@mezzanine.orchestra>
2015-02-09 14:26:45 -0500

    Job submission scripts

--- python/shoelace/scripts/RSEM-calc-expression_All_cells_in_dir.py
+++ python/shoelace/scripts/RSEM-calc-expression_All_cells_in_dir.py
@@ -35,16 +35,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--configfile", help="Path to configuration file")
 parser.add_argument("-d", "--directory", help="Directory containing *.fastq files to align")
 parser.add_argument("-r", "--reference", help="Path to reference to align to")
-
 args, unknown = parser.parse_known_args()
-bowtieargstring = ''
+rsemargstring = ''
 for i in unknown:
-    bowtieargstring = bowtieargstring + ' ' + str(i)
-bowtieargstring = bowtieargstring + ' ' 
+    rsemargstring = rsemargstring + ' ' + str(i)
+rsemargstring = rsemargstring + ' '
 
-"""
-Setup directories, files
-"""
+
+
 inputDir = None; referenceFile = None
 if args.directory:
     inputDir = args.directory
@@ -62,9 +60,10 @@ if not referenceFile:
     print "No reference file directory argument specified.  Using reference file specified in configuration file."
     referenceFile = p.RefGenomeDir
 
-"""
-Find files, generate commands and submit jobs
-"""
+
+convert_command = os.path.join(p.rsem_dir,'convert-sam-for-rsem')
+rsem_command = os.path.join(p.rsem_dir,'rsem-calculate-expression')
+
 for idir in inputDir:
     inputFilenames_1 = glob.glob(os.path.join(idir,'*_1.fastq'))
     if (len(inputFilenames_1) < 1):
@@ -72,29 +71,31 @@ for idir in inputDir:
         if (len(inputFilenames_1) > 0):
             singleEnd = True
         else:
-            sys.exit("No FASTQ files found in directory: " + idir)
+            print("No FASTQ files found in directory: " + idir + ". Assuming single-end reads")
+            singleEnd = True
     else:
-        inputFilenames_2 = []
         singleEnd = False
-    outputFilenames = []
-    for f1 in inputFilenames_1:
+    inputSAMfilenames = glob.glob(os.path.join(idir,'*.sam'))
+    for i in range(len(inputSAMfilenames)):
+        out_rsem = re.sub('\.sam','_rsem',inputSAMfilenames[i])
+        out_bam  = re.sub('\.sam','.bam',inputSAMfilenames[i])
+        out_expression = re.sub('\.sam','_expression',inputSAMfilenames[i])
+        print inputSAMfilenames[i]
         if singleEnd:
-            outputFilenames.append(re.sub('\.fastq','.sam', f1))
+            command_string = "bsub -q short -n 6 -W 12:00 -e ~/.lsbatch_errors_rsem -o lsf_output.txt \"{0} {1} {2}; {3} {4} --bam {5} {6} {7}\"".format(convert_command, inputSAMfilenames[i], out_rsem, rsem_command, rsemargstring, out_bam, referenceFile, out_expression)
         else:
-            outputFilenames.append(re.sub('_1\.fastq','.sam', f1))
-            inputFilenames_2.append(re.sub('_1\.fastq','_2.fastq', f1))
+            command_string = "bsub -q short -n 6 -W 12:00 -e ~/.lsbatch_errors_rsem -o lsf_output.txt \"{0} {1} {2}; {3} {4} --bam --paired-end {5} {6} {7}\"".format(convert_command, inputSAMfilenames[i], out_rsem, rsem_command, rsemargstring, out_bam, referenceFile, out_expression)
 
-    for i in range(len(inputFilenames_1)):
-        print inputFilenames_1[i]
-        print outputFilenames[i]
-        if singleEnd:
-            command_string = "bsub -q short -n 4 -W 12:00 -R \"rusage[mem=12000]\" -e ~/.lsbatch_errors -o lsf_output.txt \"{0} {1} {2} -n 0 -e 99999999 -l 25 -I 1 -X 2000 -a -m 15 {3} -S {4};\"".format(p.bowtie, p.RefGenomeDir, bowtieargstring, inputFilenames_1[i], outputFilenames[i])
-        else:
-            command_string = "bsub -q short -n 4 -W 12:00 -R \"rusage[mem=12000]\" -e ~/.lsbatch_errors -o lsf_output.txt \"{0} {1} {2} -n 0 -e 99999999 -l 25 -I 1 -X 2000 -a -m 15 -1 {3} -2 {4}, -S {5};\"".format(p.bowtie, p.RefGenomeDir, bowtieargstring, inputFilenames_1[i], inputFilenames_2[i], outputFilenames[i])
         try:
             os.system(command_string)
         except OSError:
             sys.exit("Command failed: " + command_string)
         print "Job {0}/{1} submitted for dir {2}".format(i+1,len(inputFilenames_1),idir)
         
+
+
+
+
+
+
 
