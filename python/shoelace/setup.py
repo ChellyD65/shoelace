eb1883526a9233629d7b2d107d28a172ef143861
Marcello DiStasio <md267@balcony.orchestra>
2015-02-17 21:13:56 -0500

    Added setup for dependencies

---
+++ python/shoelace/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shoelace',
+      version='0.2',
+      description='A python package for RNA sequence data and expression level analysis and utilities for querying the NCBI Gene Expression Omnibus (GEO) database',
+      url='http://github.com/ChellyD65/shoelace',
+      author='Marcello DiStasio',
+      license='GNU GPL',
+      packages=['shoelace'],
+      dependency_links=["git+https://github.com/wmayner/pyemd@develop#egg=pyemd"]
+      zip_safe=False)
