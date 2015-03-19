445324f15c84c10b5f465a0d57d05d510cc61aae
Marcello DiStasio <md267@mezzanine.orchestra>
2015-03-18 20:19:38 -0400

    EMD cell pair plotting for multiple files and real/virtual

--- python/shoelace/lib/EMDpairs.py
+++ python/shoelace/lib/EMDpairs.py
@@ -36,26 +36,24 @@ class EMDpairs:
         self.u = Utilities.Utilities()
         self.saveAsPDF = True
 
-    def setup(self,filenames, normalizeByShuffled, virtualOnly):
-        
+    def setup(self,filenames, virtual):
+
+        self.run_real = True
+        self.run_virtual = virtual
 
         self.data = []
         for f in range(len(filenames)):
             d = np.load(filenames[f],'r')
-            if normalizeByShuffled:
-                tsel_r, tsel_v = self.u.get_tsel_both(d)
-                tsel_r_norm = (tsel_r - np.mean(tsel_r, axis=0))/np.std(tsel_r, axis=0)
-                tsel_v_norm = (tsel_v - np.mean(tsel_v, axis=0))/np.std(tsel_v, axis=0)
-                self.data.append(tsel_r_norm - tsel_v_norm)
-            else:
-                if virtualOnly:
-                    self.data.append(d['tsel_v'])
-                else:
-                    self.data.append(d['tsel_r'])
-            print(self.data[-1].shape)
+            dataset = []
+            if self.run_virtual:
+                dataset.append([d['tsel_v']])
+            if self.run_real:
+                dataset.append([d['tsel_r']])
+            self.data.append(dataset)
+        print(len(self.data))
+        print(len(self.data[0]))
 
     def run(self):
-
 
         if self.saveAsPDF:
             timelabel = time.strftime("%m_%d_%Y_-_%H_%M_%S")
@@ -64,24 +62,34 @@ class EMDpairs:
             print("Saving plots to PDF: " + pdffn)
 
         mpl.rcParams['figure.figsize'] = (40.0, 60.0)
-        fig, axes = plt.subplots(len(self.data))
+        nfigrow = len(self.data)
+        nfigcol = len(max(self.data, key=len))
+        fig, axes = plt.subplots(nfigrow,nfigcol)
+        print(axes.shape)
 
-        f = 0
-        for tumor_set in self.data:
-            histog = np.apply_along_axis(lambda x: np.histogram(x,256)[0], 1, tumor_set)
-            em = self.emd_allrows(histog)
+        r = 0
+        for dataset in self.data:
+            c = 0
+            for dd in dataset:
+                tumor_set = dd[0]
+                print(tumor_set.shape)
+                histog = np.apply_along_axis(lambda x: np.histogram(x,256)[0], 1, tumor_set)
+                em = self.emd_allrows(histog)
+                """
+                #Next, sort rows of em by their sums, to look nice
+                """
+                b = -1*np.sum(em, axis = 0)
+                idx = b.argsort()
 
-            """
-            Next, should sort rows of em by their sums, to look nice
-            """
+                im = axes[r,c].imshow(em[:,idx],aspect='equal');
+                topval = np.max(em)
+                #topval = 2700
+                cbar = plt.colorbar(im, ax=axes[r,c], ticks=np.round(np.arange(np.min(em),topval,100)*2)/2)
+                cbar.ax.set_yticklabels(map(str, np.round(np.arange(np.min(em),topval,100)*2)/2))# vertically oriented colorbar
+                cbar.set_label('EMD over gene expression distribution')
 
-
-            cax = axes[f].imshow(em,aspect='equal');
-            cbar = plt.colorbar(cax, ticks=np.round(np.arange(np.min(em),np.max(em),100)*2)/2)
-            cbar.ax.set_yticklabels(map(str, np.round(np.arange(np.min(em),np.max(em),100)*2)/2))# vertically oriented colorbar
-            cbar.set_label('EMD over gene expression distribution')
-
-            f=f+1
+                c=c+1
+            r=r+1
 
         if self.saveAsPDF:
             pp.savefig()
@@ -105,9 +113,8 @@ class EMDpairs:
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--loadfile",  help="Load a previous output *.npz file")
-    parser.add_argument("--normalize", help="Normalize the real dataset by the shuffled dataset", default = False)
-    parser.add_argument("--virtual",   help="Plot the virtual cells", default = False)
+#    parser.add_argument("--loadfile",  help="Load a previous output *.npz file")
+    parser.add_argument("--virtual",   help="Plot the virtual cells also", default = False)
     parser.add_argument("--filelist",  help="Load previous output *.npz files from list passed on command line", nargs='+')
     args = parser.parse_args()
 
@@ -115,11 +122,9 @@ if __name__ == "__main__":
     if args.filelist:
         e = EMDpairs()
 #        t = time.strftime("%m_%d_%Y_-_%H_%M_%S")
-        e.setup(args.filelist, bool(int(args.normalize)), bool(int(args.virtual)))
+        e.setup(args.filelist, bool(int(args.virtual)))
         print(e.run())
-
-
         
     else:
-        print("If calling this file directly, you must specify the \"--loadfile\" <path> or \"--filelist\" <files> argument with a path to the *.npz file containing the processed data.")
+        print("If calling this file directly, you must specify the \"--filelist\" <files> argument with a path to the *.npz file containing the processed data.")
     
